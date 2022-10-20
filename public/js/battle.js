b0f14f0082eacb5d68b175c9e956773d0c118dd4
deand925 <97762768+deand925@users.noreply.github.com>
2022-10-20 13:54:30 -0700

    test

---
+++ public/js/battle.js
@@ -0,0 +1,69 @@
+
+let hp = document.currentScript.getAttribute('hp');
+let level = document.currentScript.getAttribute('level');
+
+//-------TESTING PURPOSES ONLY-------------
+const $lowerHealth = document.getElementById('decrement-health-test');
+const $levelUp = document.getElementById('increase-level-test');
+
+console.log(hp);
+console.log(level);
+//------------------------------------------
+const newMonster = async function () {
+  try {
+    const res = await fetch('/api/monsters/:id', {
+      method: "GET",
+    }),
+    if (res.ok) {
+      console.log(res)
+    } else {
+      alert('error')
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+const levelUp = async function () {
+  try {
+    const res = await fetch('/api/users/level-up', {
+      method: 'POST',
+    });
+
+    if (res.ok) {
+      console.log('level up');
+      location.reload();
+    } else {
+      alert('error');
+    }
+  } catch (error) {
+    console.log(error);
+  };
+}
+
+const updateHP = async function () {
+  try {
+    const hpValue = (-5);
+    const res = await fetch('/api/users/update-hp', {
+      method: 'POST',
+      body: JSON.stringify({
+        hpChange: hpValue
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      },
+    });
+
+    if (res.ok) {
+      console.log('update HP');
+      location.reload();
+    } else {
+      alert('error');
+    }
+  } catch (error) {
+    console.log(error);
+  };
+}
+
+$levelUp.addEventListener('click', levelUp);
+$lowerHealth.addEventListener('click', updateHP);
+newMonster();
