521dea905cb6ac0062f6e75bf48da59eedc446af
palminski <williambolls98@gmail.com>
2022-10-21 10:12:30 -0700

    riddles displayed with monster and can be advanced to next riddle

--- public/js/battle.js
+++ public/js/battle.js
@@ -1,14 +1,18 @@
 
 let hp = document.currentScript.getAttribute('hp');
 let level = document.currentScript.getAttribute('level');
+let riddleIndex = document.currentScript.getAttribute('riddleIndex');
 
 const $monsterImage = document.getElementById('monster-image');
 const $monsterName = document.getElementById('monster-name');
 const $monsterDescription = document.getElementById('monster-description');
+const $monsterRiddle = document.getElementById('monster-riddle');
 
+const correctAnswer = 'test';
 //-------TESTING PURPOSES ONLY-------------
 const $lowerHealth = document.getElementById('decrement-health-test');
 const $levelUp = document.getElementById('increase-level-test');
+const $riddleUp = document.getElementById('increase-riddle-index');
 
 console.log(hp);
 console.log(level);
@@ -20,9 +24,14 @@ async function displayMonster() {
   const monster = await res.json();
   console.log(monster);
 
+  if (typeof monster.riddles[riddleIndex] == 'undefined') {
+    levelUp();
+    return;
+  }
   $monsterImage.src = `/images/${monster.image}`;
   $monsterName.textContent = monster.name;
   $monsterDescription.textContent = monster.description;
+  $monsterRiddle.textContent = monster.riddles[riddleIndex].question;
 }
 //------------------------------------------
 
@@ -41,7 +50,6 @@ const fightMonster = async function () {
     console.log(err)
   }
 }
-
 fightMonster();
 
 const levelUp = async function () {
@@ -84,9 +92,27 @@ const updateHP = async function () {
     console.log(error);
   };
 }
+const nextRiddle = async function () {
+  try {
+    const res = await fetch('/api/users/next-riddle', {
+      method: 'POST',
+    });
+
+    if (res.ok) {
+      console.log('riddle up');
+      location.reload();
+    } else {
+      alert('error');
+    }
+  } catch (error) {
+    console.log(error);
+  };
+}
 //---------RUNS WHEN PAGE LOADED---------
 displayMonster();
 
 $levelUp.addEventListener('click', levelUp);
 $lowerHealth.addEventListener('click', updateHP);
+$riddleUp.addEventListener('click', nextRiddle);
+
 
