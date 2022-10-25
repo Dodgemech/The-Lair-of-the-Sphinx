444dd0167e3b1332b3914134428776be2df3f445
deand925 <97762768+deand925@users.noreply.github.com>
2022-10-25 14:51:30 -0700

    removed more scores related data and updated game over page with if statements for it the player wins or loses

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,6 +1,14 @@
 
 
 module.exports = { 
+    ifLess : (hp) => {
+       if(hp <= 0){
+           return true;
+       } 
+       else
+       return false;
+    },
+
     ifEqual : (string, value) => {
         if (string === value) {
             return true;
