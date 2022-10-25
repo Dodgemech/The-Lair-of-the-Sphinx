49f191e3ab897a9da82f785a29718ed64ab0fec3
MLanfear <maxjlanfear@gmail.com>
2022-10-24 20:16:54 -0700

    working input barrrrrrr

--- public/js/battle.js
+++ public/js/battle.js
@@ -7,18 +7,17 @@ const $monsterName = document.getElementById('monster-name');
 const $monsterDescription = document.getElementById('monster-description');
 const $monsterRiddle = document.getElementById('monster-riddle');
 
+let riddleAnswer;
+
 // Test ON INPUT BTN
 const $answerInput = document.getElementById('answerInput');
 const $answerInputBtn = document.getElementById('answerInputBtn');
-// const $answerData = document.getElementById('answers');
 
-// const correctAns = 0;
 
-// const correctAnswer = 'test';
 //-------TESTING PURPOSES ONLY-------------
-const $lowerHealth = document.getElementById('decrement-health-test');
+// const $lowerHealth = document.getElementById('decrement-health-test');
 const $levelUp = document.getElementById('increase-level-test');
-const $riddleUp = document.getElementById('increase-riddle-index');
+// const $riddleUp = document.getElementById('increase-riddle-index');
 
 
 console.log(hp);
@@ -27,7 +26,6 @@ console.log(level);
 
 //------DISPLAY MONSTER ON PAGE-------------
 async function displayMonster() {
-// const displayMonster = async function() {
   const res = await fetch(`/api/monsters/${level}`);
   const monster = await res.json();
   console.log(monster); 
@@ -40,6 +38,7 @@ async function displayMonster() {
   $monsterName.textContent = monster.name;
   $monsterDescription.textContent = monster.description;
   $monsterRiddle.textContent = monster.riddles[riddleIndex].question;
+  riddleAnswer = monster.riddles[riddleIndex].answers;
 }
 
 //------------------------------------------
@@ -48,36 +47,26 @@ async function displayMonster() {
 
 // TEST OF ANSWER INPUT/BTN
 
-const submitAnswer = async function() {
-  asyncfunction.preventDefault();
-  console.log();
+const submitAnswer = async function(event) {
+  event.preventDefault();
+  let answerValue = $answerInput.value;
+  if (answerValue.trim().length == 'undefined') {
+    alert('Answer must be provided');
+    return;
+  }
+  console.log(answerValue);
+
   try {
-    const answerValue = $answerInput.value;
 
-    const res = await fetch('/api/users/current/now', {
-      method: 'POST',
-    })
-    if (answerValue.text() === $answerInputBtn.text()) {
-      return;
-    };  
-  
-    if (res.ok) {
-      const user = await res.json();
-      console.log(user)
-    return response.json();
-    
+    if (answerValue === riddleAnswer) {
+      alert('Answer Correct!');
+      nextRiddle();    
 
-    // location.reload();
     } else {
-      alert('error');
+      alert('Boo! Answer Incorrect')
+      updateHP();
     }
     
-    if (answerValue.trim().length == 'undefined') {
-      alert('Answer must be provided');
-      return;
-    }
-    console.log(answerValue);
-  
   } catch (error) {
     console.log(error);
   };
@@ -142,185 +131,11 @@ const nextRiddle = async function () {
   };
 }
 
-// // TEST OF ANSWER INPUT/BTN
-
-// // $answerBtnInput.addEventListener( 'click', async (event) => {
-// const submitAnswer = async function () {
-//   asyncfunction.preventDefault();
-
-//   const answerValue = $answerInput.value;
-//   console.log(submitAnswer);
-//   // const questionInput = $answerData.value;
-
-//   if (answerValue.trim().length == 0) {
-//     alert('Answer must be provided');
-//     return;
-//   }
-//   console.log(answerValue);
-//   // if (questionInput.trim().length === 0) {
-//   //   alert('');
-//   //   return;
-//   // }
-
-//   try {
-//     const res = await fetch('/api/users/current/now', {
-//       method: 'GET',
-//       body: JSON.stringify({
-//         answer: answerValue
-//       }),
-//       headers: {
-//         'Content-Type': 'application/json'
-//       } 
-//     })
-//     if (res.ok) {
-//       const user = await res.json();
-//       console.log(user)
-//       location.reload();
-//   } else {
-//     alert('error');
-//   }
-//   } catch (error) {
-//     console.log(error);
-//   }
-// }
-// // submitAnswer();
-  
-  
-  // try {
-  //   // const checkAnswer = async function () {
-  //     // let out = $('.output').value;
-  //   if (question.$(answerValue) === 0); {
-          
-  //   } else {
-  //     alert('error');
-  //   }
-
-
-  
-// displayMonster()
-// const fightMonster = async function () {
-//   try {
-//     const res = await fetch('/api/users/current/now', {
-//       method: 'GET',
-//     });
-    
-
-//     if (res.ok) {
-//     const user = await res.json();
-//     console.log(user)
-//     } else {
-//       alert('error');
-//     }
-    
-//   } catch (error) {
-//     console.log(error);
-//   };
-// }
-// fightMonster();
-
-    // };
-    // } catch (error) {
-    //   console.log(answerValue);
-    //   }
-
-
-// const nextRiddle = async function () {
-//   console.log(nextRiddle);
-//   try {
-//     const res = await fetch('/api/users/next-riddle', {
-//       method: 'POST',
-//     });
-
-//     if (res.ok) {
-//       console.log('riddle up');
-//       location.reload();
-
-//     } else {
-//       alert('error');
-//     }
-//   } catch (error) {
-//     console.log(error);
-//   };
-// };
-
-
-
-    // if (currentQuestion >= questions.length) {
-    //     showSummary();
-    // } else {
-    //     showQuestion();
-    // }
-
-    // function () {
-    //     $('.output').val('');
-    // };
-  
-
-
-  // try {
-  //   const res = await fetch('/api/users/riddle', {
-  //     method: 'POST',
-  //     body: JSON.stringify({
-  //       userAnswer: answerValue
-  //     }),
-  //     headers: {
-  //       'Content-Type': 'application/json'
-  //     },
-  //   });
-    
-  //   const checkAnswer = $answerInput;
-  
-  //     if (checkAnswer[riddleIndex].answers === checkAnswer[riddleIndex].answerValue) {
-  //         // correct answer, move to next riddle
-  //         correctAns++;
-  //         // console.log(correctAns);
-  //         answerCheck.textContent = "Bravo!";
-  //     } else {
-  //         // wrong answer, deduct 5 second from HP
-  //         updateHP = -5;
-  //         updateHP.textContent = hpValue;
-  //         answerCheck.textContent = "Womp womp! The correct answer is: " + questions[riddleIndex].answers;
-  //     };
-  
-  //     riddleIndex++;
-    
-  //     console.log(checkAnswer);
-  //     console.log(answers);
-    
-  
-
-//   } catch (error) {
-//     console.log(error);
-//   };
-// })
-
-
-  
-  // //  
-  // if (res.ok) {
-  //   console.log('all signed up!');
-  //   document.location.replace('/');
-  // } else {
-  //   alert('Unable to sign up!');
-  // }
-
-
-
-
-
-
-
-
-
-
-
-
 //---------RUNS WHEN PAGE LOADED---------
-// displayMonster();
 displayMonster();
 $levelUp.addEventListener('click', levelUp);
-$lowerHealth.addEventListener('click', updateHP);
-$riddleUp.addEventListener('click', nextRiddle);
+// $lowerHealth.addEventListener('click', updateHP);
+// $riddleUp.addEventListener('click', nextRiddle);
 $answerInputBtn.addEventListener('click', submitAnswer);
 
 
